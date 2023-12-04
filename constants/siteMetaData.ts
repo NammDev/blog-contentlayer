6968788d0ccda33d4c3ab4e51db3eb712b83f21d
Nam <namdeveloper.ca@gmail.com>
2023-12-04 11:17:11 +0700

    Say my name

--- constants/siteMetaData.ts
+++ constants/siteMetaData.ts
@@ -1,7 +1,7 @@
 // please update required information
 export const siteMetadata = {
   title: 'Next.js Blog With Tailwind CSS and Contentlayer',
-  author: 'CodeBucks',
+  author: 'NammDev',
   headerTitle: 'Next.js Blog',
   description: 'A blog created with Next.js, Tailwind.css and contentlayer.',
   language: 'en-us',
@@ -9,12 +9,12 @@ export const siteMetadata = {
   siteUrl: 'https://create-blog-with-nextjs.vercel.app', // your website URL
   siteLogo: '/logo.png',
   socialBanner: '/social-banner.png', // add social banner in the public folder
-  email: 'codebucks27@gmail.com',
-  github: 'https://github.com/codebucks27',
-  twitter: 'https://twitter.com/code_bucks',
+  email: 'namdeveloper.ca@gmail.com',
+  github: 'https://github.com/NammDev',
+  twitter: 'https://twitter.com/namdeveloper_ca',
   facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com/codebucks',
-  linkedin: 'https://www.linkedin.com/in/codebucks/',
+  youtube: 'https://youtube.com',
+  linkedin: 'https://www.linkedin.com',
   dribbble: 'https://www.dribbble.com',
   locale: 'en-US',
 }
